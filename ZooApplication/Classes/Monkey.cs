1fa0c507c06ec6023dc2ca3c2e60c2d6be877067
Bram Jager <bram.jager@itvitaelearning.nl>
2021-05-06 11:00:01 +0200

    Created Animal classes and subclasses

---
+++ ZooApplication/Classes/Monkey.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+namespace ZooApplication
+{
+	public class Monkey : Animal
+	{
+		public Monkey()
+		{
+		}
+	}
+}
