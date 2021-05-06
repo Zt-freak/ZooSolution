d3fc3cc24eba8830b05e294f54bb1d5474684b4d
Zt-freak <bnkho98@hotmail.com>
2021-05-06 14:57:41 +0200

    added ASP.NET MVC project

---
+++ Zoo.ASP/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Zoo.ASP
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["Message"] = "yeet";
+            return View();
+        }
+    }
+}
